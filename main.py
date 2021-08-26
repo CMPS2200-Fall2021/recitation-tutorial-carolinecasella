113f8c83b5cbf7ae9a9d881794c7acafbb323198
carolinecasella <69976540+carolinecasella@users.noreply.github.com>
2021-08-26 10:18:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 def sum_of_squares(a):
-	pass
+	sum = 0
+	for num in a:
+		square = num ** 2
+		sum += square
+	return sum
 
 def test_one():
     assert sum_of_squares([1,2,3]) == 14
+
+test_one()
